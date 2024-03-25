310480b2d1f2b90da207f3839798e852f5a22024
laith alarmouti <laithalarmouti@gmail.com>
2024-03-25 09:31:45 +0300

    first commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AppUser> Users {get; set;}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<AppUser>().HasData(
+            new AppUser { Id = 1, UserName = "Laith" },
+            new AppUser { Id = 2, UserName = "Leen"  },
+            new AppUser { Id = 3, UserName = "Yousef" },
+            new AppUser { Id = 4, UserName =  "Jamal"},
+            new AppUser { Id = 5, UserName =  "Ghadeer"}
+        );
+    }
+}
---
+++ API/Data/Migrations/20240323141734_SeedTable2.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedTable2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "UserName" },
+                values: new object[,]
+                {
+                    { 1, "Laith" },
+                    { 2, "Leen" },
+                    { 3, "Yousef" },
+                    { 4, "Jamal" },
+                    { 5, "Ghadeer" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
