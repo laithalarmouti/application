653edecea81089728d5adc87ea64865cbdff7530
Laith <Laithalarmouti@gmail.com>
2024-03-29 16:34:53 +0300

    72

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc0f60ee19bfdd9ee2b8c2aeda53384908f5acad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+234465b78eb71ede3879ed1cd52262046277cf61")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
