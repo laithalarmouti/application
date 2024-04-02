e237a09ae241335078b30cd61cd921b86ef744c0
laith alarmouti <laithalarmouti@gmail.com>
2024-04-02 13:06:16 +0300

    122

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,8 +1,7 @@
-﻿using API.DTOs;
-using API.Entities;
+﻿using System.Security.Claims;
+using API.DTOs;
 using API.Interfaces;
 using AutoMapper;
-using AutoMapper.Execution;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -34,4 +33,18 @@ public async Task<ActionResult<MemberDto>> GetUser(string username)
 {
     return await _userRepository.GetMemberAsync(username);
 }
+
+[HttpPut]
+public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+{
+var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+var user = await _userRepository.GetUserByUsernameAsync(username);
+if(user==null) return NotFound();
+
+_mapper.Map(memberUpdateDto, user);
+
+if(await _userRepository.SaveAllAsync()) return NoContent();
+return BadRequest("Failed to update user");
+}
+
 }
---
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -0,0 +1,9 @@
+import { CanDeactivateFn } from '@angular/router';
+import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+
+export const preventUnsavedChangesGuard: CanDeactivateFn<MemberEditComponent> = (component) => {
+  if(component.editForm?.dirty){
+    return confirm('Are You Sure You Want To Continue? Any Unsaved Changes Will Be Lost')
+  }
+  return true;
+};
