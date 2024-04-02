a2aa2014eacd7a6e30295a03487abf38f2a4401f
laith alarmouti <laithalarmouti@gmail.com>
2024-04-02 15:10:53 +0300

    128

---
+++ API/Interfaces/IPhotoService.cs
@@ -0,0 +1,16 @@
+﻿using CloudinaryDotNet.Actions;
+
+namespace API.Interfaces;
+
+public interface IPhotoService
+{
+
+Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+Task<DeletionResult> DeletePhotoAsync(string publicId);
+
+
+
+
+
+
+}
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50ebae83eebb873fed5d0ebdc405fb5faf0eb110")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e237a09ae241335078b30cd61cd921b86ef744c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
