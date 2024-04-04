6a2f6bd7b446d1b3fbaecd457640c194a25e72e7
laith alarmouti <laithalarmouti@gmail.com>
2024-04-04 14:33:02 +0300

    152

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb311c47c4e75410663c442d42a7182c382dacd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce54777454b58092596e534a15e27d7b2390bccc")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
